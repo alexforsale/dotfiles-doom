99db501081c7505f8c5d31b6acbe71d69322a90f
alexforsale <alexforsale@yahoo.com>
2022-08-15 19:31:06 +0700

    font and theme

--- config.el
+++ config.el
@@ -28,11 +28,13 @@
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
 ;; refresh your font settings. If Emacs still can't find your font, it likely
 ;; wasn't installed correctly. Font issues are rarely Doom issues!
+(setq doom-font (font-spec :family "Fira Code Retina" :size 10)
+      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 11))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-henna)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
