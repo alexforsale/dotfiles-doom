e13fcd0c18ebd281ac93a15824da12e8b10a8a2c
alexforsale <alexforsale@yahoo.com>
2023-05-09 14:01:01 +0700

    org-directory: prioritize Syncthing directory

--- config.el
+++ config.el
@@ -58,15 +58,14 @@
 ;; change `org-directory'. It must be set before org loads!
 (cond
  ((file-directory-p
+   (expand-file-name "Sync/org" (getenv "HOME")))
+  (setq org-directory (expand-file-name "Sync/org" (getenv "HOME"))))
+ ((file-directory-p
    (expand-file-name "Documents/google-drive/org" (getenv "HOME")))
   (setq org-directory (expand-file-name "Documents/google-drive/org" (getenv "HOME"))))
  ((file-directory-p
    (expand-file-name "Dropbox/org" (getenv "HOME")))
-  (setq org-directory (expand-file-name "Dropbox/org" (getenv "HOME"))))
- ((file-directory-p
-   (expand-file-name "Sync/org" (getenv "HOME")))
-  (setq org-directory (expand-file-name "Sync/org" (getenv "HOME"))))
- )
+  (setq org-directory (expand-file-name "Dropbox/org" (getenv "HOME")))))
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
 ;;
